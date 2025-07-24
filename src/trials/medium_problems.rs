e4a90e76ea4c62ec28a5ae8361fbfc6878d3241e
upamanyu <uchakravarty@ti.com>
2025-07-24 06:09:39 +0530

    refactor: memory & runtime optimizations 3

--- src/trials/medium_problems.rs
+++ src/trials/medium_problems.rs
@@ -1,11 +1,8 @@
-use std::collections::HashMap;
-
 /*
     PROBLEM: Valid Subsequences II
-    SOLUTION: METHOD II (Dynamic Programming)
-    LEETCODE STATS: Runtime = 286ms | Memory = 5.49MB
+    LEETCODE STATS: Runtime = 42ms | Memory = 4.1MB
 
-    METHODOLOGY
+    SOLUTION:
     - start with second-last element and then repeat below for every element before it
     - let the starting element be 1st element and the next element in each iteration be 2nd element
     - create a map with keys as result and value as an internal map
@@ -17,9 +14,12 @@ use std::collections::HashMap;
     - keep updating value of a max variable whenever length gets incremented
     - return max as the final result
 
-    OPTIMIZATIONS
-    - Using u16 instead of usize or i32 (memory: 13MB -> 5.3MB, runtime: 0.7s -> 0.45s)
+    OPTIMIZATIONS:
+    - Using u16 instead of i32 wherever possible (memory: 13MB -> 5.3MB, runtime: 0.7s -> 0.45s)
     - Using a fixed-size array instead of HashSet (memory: 5.3MB -> 5.5MB, runtime: 0.45s -> 0.3s)
+    - Using a fixed-size array instead of HashMap (memory: 5.5MB -> 4.12MB, runtime: 0.3s -> 0.04s)
+        - this specifically causes the main thread to go out of stack on local machine for k = 1000
+        - setting sizes to 100 so that main thread doesn't go out of stack
 */
 pub fn medium_problem_1() {
     println!("medium problem 1 (Dynamic programming) - Valid subsequence II");
@@ -29,46 +29,39 @@ pub fn medium_problem_1() {
     let k = 2;
 
     // initializations
-    let mut extmap: HashMap<u16, HashMap<u16, u16>> = HashMap::new();
+    let mut extmap = [[0u16;100];100];
     let mut max_len = 2;
     let mut complexity_count = 0;
 
     // implementation
-    for ext_index in (0..(nums.len() - 1) as u16).rev() {
-        let mut used_vals = [0u16;1000];
+    for ext_index in (0..nums.len() - 1).rev() {
+        let mut used_vals = [0u16;100];
 
-        for int_index in (ext_index + 1)..nums.len() as u16 {
-            let res = ((nums[ext_index as usize] + nums[int_index as usize]) % k) as u16;
+        for int_index in (ext_index + 1)..nums.len() {
+            let res = ((nums[ext_index] + nums[int_index]) % k) as usize;
 
             // skip if we already have this result and this is guaranteed to be equal or shorter
-            if used_vals[res as usize] == 0 {
-                used_vals[res as usize] = 1;
-                complexity_count += 1;
+            if used_vals[res] == 1 {
+                continue;
+            }
+            used_vals[res] = 1;
+            complexity_count += 1;
 
-                // insert into data structure
-                if let Some(intmap) = extmap.get_mut(&res) {
-                    if let Some(curr_len) = intmap.get(&int_index) {
-                        // if index of 2nd element in internal map, add 1 to corresponding value as length
-                        let actual_len = *curr_len + 1;
-                        intmap.insert(ext_index, actual_len);
-                        if actual_len > max_len {
-                            max_len = actual_len;
-                        }
-                    } else {
-                        // if index of 2nd element not in internal map, set length 2 with index of 1st element
-                        intmap.insert(ext_index, 2);
-                    }
-                } else {
-                    // if external map doesn't have key as result, create a new entry with length 2
-                    let mut int_map1 = HashMap::new();
-                    int_map1.insert(ext_index, 2);
-                    extmap.insert(res, int_map1);
+            // insert into data structure
+            if extmap[res][int_index] != 0 {
+                // if index of 2nd element in internal map, add 1 to corresponding value as length
+                extmap[res][ext_index] = extmap[res][int_index] + 1;
+                if extmap[res][ext_index] > max_len {
+                    max_len = extmap[res][ext_index];
                 }
+            } else {
+                // if index of 2nd element not in internal map, set length 2 with index of 1st element
+                extmap[res][ext_index] = 2;
             }
         }
     }
 
-    println!("Maps = {:?}", extmap);
+    // results
     println!("Max length = {}", max_len);
     println!("Comlexity = {}", complexity_count);
 }
