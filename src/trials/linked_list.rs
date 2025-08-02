54f913aa786804e5bacb1511468378b9742071fa
upamanyu <uchakravarty@ti.com>
2025-08-03 01:37:57 +0530

    refactor: create linked list tail ref

--- src/trials/linked_list.rs
+++ src/trials/linked_list.rs
@@ -1,22 +1,39 @@
-struct Node {
+/*
+ * Structure for the linked list node
+ */
+struct LinkedListNode {
     val: i32,
-    next: Option<Box<Node>>
+    next: Option<Box<LinkedListNode>>
 }
-impl Node {
+
+impl LinkedListNode {
+    /*
+     * Create new instance of node with a value
+     */
     fn new(val: i32) -> Self {
         Self {val, next: None}
     }
-    fn push(&mut self, val: i32) {
+
+    /*
+     * Push an element to the linked list while keeping the head reference intact
+     * Also returns a mutable reference of the current tail after push
+     */
+    fn push(&mut self, val: i32) -> &mut Box<LinkedListNode> {
         match self.next {
             Some(ref mut next_node) => {
-                next_node.push(val);
+                next_node.push(val)
             },
             None => {
-                self.next = Some(Box::new(Node::new(val)));
+                self.next = Some(Box::new(LinkedListNode::new(val)));
+                self.next.as_mut().unwrap()
             }
         }
     }
-    fn pop(&mut self) -> Option<Box<Node>> {
+
+    /*
+     * Pop tail element from the linked list while keeping the head reference intact
+     */
+    fn pop(&mut self) -> Option<Box<LinkedListNode>> {
         if self.next.is_some() {
             let next_node = self.next.as_ref().unwrap();
             if next_node.next.is_some() {
@@ -25,19 +42,27 @@ impl Node {
                 // technically a cloned value of the pop
                 let actual_val = next_node.val;
                 self.next = None;
-                Some(Box::new(Node::new(actual_val)))
+                Some(Box::new(LinkedListNode::new(actual_val)))
             }
         } else {
             // cannot pop if only one element
             None
         }
     }
+
+    /*
+     * Returns the length of the linked list while keeping the head reference intact
+     */
     fn length(&self) -> usize {
         1 + match self.next {
             Some(ref next_node) => next_node.length(),
             None => {0}
         }
     }
+
+    /*
+     * Returns a string representation of the linked list while keeping the head reference intact
+     */
     fn print(&self) -> String {
         let mut display_str = self.val.to_string();
         let next_str = match self.next {
@@ -52,16 +77,19 @@ impl Node {
  * TODO:
  * - PUSH (DONE)
  * - POP (DONE)
- * - TAIL REF
- * - CIRCULAR
+ * - TAIL REF (PARTIALLY - but currently kind of useless)
  * - DOUBLY-LINKED
+ * - CIRCULAR
  */
 pub fn linked_list_trial() {
     // pushing and length
-    let mut head = Node::new(1);
+    let mut head = LinkedListNode::new(1);
     head.push(2);
     println!("list size = {}, vals = {}", head.length(), head.print());
-    head.push(3);
+
+    // get tail reference from push
+    let mut tail = head.push(3);
+    println!("tail = {}", tail.val);
     println!("list size = {}, vals = {}", head.length(), head.print());
 
     // popping while preserving preference
